3c17e57adeba155f8b48d9a5743eb0709d26bdc7
phaulty <phaulty@example.com>
2026-07-01 14:27:29 -0600

    fix: NoneType error when candidate_pool_seeds is missing in radio launch

--- bot/views.py
+++ bot/views.py
@@ -627,7 +627,7 @@ class RadioView(discord.ui.View):
             "voice_channel_id": voice_channel.id,
             "text_channel_id": channel_id,
             "use_dj": use_dj,
-            "candidate_pool": [{"track": t, "base_score": 50, "age": 0} for t in self.playlist_data.get("candidate_pool_seeds", [])],
+            "candidate_pool": [{"track": t, "base_score": 50, "age": 0} for t in (self.playlist_data.get("candidate_pool_seeds") or [])],
         }
         set_radio_state(guild_id, state_dict)
 
